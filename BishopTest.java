38edf7cf8fdc945bb4349b73c27244cedb3d7160
Jack <jack3@vt.edu>
2013-04-23 17:48:27 -0400

    Bishop test class
    created a bishop test class

---
+++ BishopTest.java
@@ -0,0 +1,17 @@
+package model;
+
+/**
+ * // -------------------------------------------------------------------------
+/**
+ *  The test class for the bishop class. It helps find code that isn't working
+ *  and cn be used for further debugging
+ *
+ *  @author Jack
+ *  @author Loran
+ *  @author Robert
+ *  @version Apr 23, 2013
+ */
+public class BishopTest
+{
+
+}
